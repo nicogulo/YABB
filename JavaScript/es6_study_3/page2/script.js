1f3d825ed35aff3225917a0f30e38137b8b77f3b
nicogulo <nicogulobelas@gmail.com>
2021-06-01 23:07:08 +0700

    add ES6 Study 3

---
+++ JavaScript/es6_study_3/page2/script.js
@@ -0,0 +1,10 @@
+// Tetapkan function ke constant hello
+const hello  = function(){
+  console.log("Halo!");
+  console.log("Saya Nico");
+}
+
+
+// Panggil function yang ditetapkan di constant hello
+
+hello();  
