6b05a08d17f68269cbe30c6a0b824fafb238fe93
nicogulo <nicogulobelas@gmail.com>
2021-06-18 20:40:34 +0700

    add React JS Study I

---
+++ React/react_study_1/page5/App.js
@@ -0,0 +1,15 @@
+// Import React
+import React from 'react';
+
+// Nyatakan class App
+
+	class App extends React.Component {
+	  render() {
+	    return (
+	      <h1>Hello React</h1>
+	    );
+	  }
+	}
+// Export class App 
+
+export default App;
