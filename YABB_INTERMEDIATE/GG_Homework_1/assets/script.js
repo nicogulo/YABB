5eb2cfda53b6bd595531ddec6cec10519afb09b1
nicogulo <nicogulobelas@gmail.com>
2021-07-06 19:17:19 +0700

    add button event

--- YABB_INTERMEDIATE/GG_Homework_1/assets/script.js
+++ YABB_INTERMEDIATE/GG_Homework_1/assets/script.js
@@ -1,7 +1,5 @@
-// selection element
-let btnSubmit = document.getElementById('btn-submit');
+const btnSubmit = document.getElementById('btn-submit');
 
-// do something if the button click
 btnSubmit.addEventListener('click', () => {
   alert('You just played Aimer Song`s');
 });
